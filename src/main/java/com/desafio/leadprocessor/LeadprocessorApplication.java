ee3d028076885133748694225b68e9afcd5bc8b8
Guilherme Gomes <guigomes94@outlook.com>
2026-05-06 22:57:03 -0300

    Start project

---
+++ src/main/java/com/desafio/leadprocessor/LeadprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.leadprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadprocessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/leadprocessor/LeadprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.leadprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
